6f56ad11af25bc1c8bb29b72d968ee6ceb9e8d39
Pekar Aleksey <fenix_al@tut.by>
2025-11-24 14:43:26 +0300

    setting up & structuring project

---
+++ TaskTurner/Models/Task.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTurner.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsComplete { get; set; }
+        public TimeSpan Timer { get; set; }
+
+        public TaskState TaskState { get; set; }
+        public TaskImportance TaskImportance { get; set; }
+        public TaskCategory TaskCategory { get; set; }
+    }
+
+    public enum TaskState
+    {
+        InProgress,
+        Complete,
+        NotStarted,
+        Late,
+        Archived,
+        Deleted
+    }
+
+    public enum TaskCategory
+    {
+        Work,
+        Personal,
+        Home,
+        HealthWelbeing,
+        Finance,
+        Shopping,
+        SocialFamily,
+        Education,
+        Travel,
+        Errands,
+        HobbiesLeisure,
+        VolunterringCommunity,
+        BirthdaysAnniversaries,
+        Projects,
+        LongTermGoals
+    }
+
+    public enum TaskImportance
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+    }
+}
